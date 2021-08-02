240658029609db3c300c87d3d40d07d9cfcfd644
Pradheep <46066018+Riopradheep007@users.noreply.github.com>
2021-08-02 19:48:25 +0530

    Update flowerbed.py

--- Easy/flowerbed.py
+++ Easy/flowerbed.py
@@ -6,13 +6,15 @@ You have a long flowerbed in which some of the plots are planted, and some are n
 Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule.
 
 
-Eg1:
-Input: flowerbed = [1,0,0,0,1], n = 1
-Output: true
+Eg 1:
 
-Eg2:
-Input: flowerbed = [1,0,0,0,1], n = 2
-Output: false
+     Input: flowerbed = [1,0,0,0,1], n = 1
+     Output: true
+
+Eg 2:
+
+     Input: flowerbed = [1,0,0,0,1], n = 2
+     Output: false
 
 '''
 
