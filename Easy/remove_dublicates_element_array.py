971ee4ef3c1553ba647ea9fa24009d22a86e410b
Pradheep <46066018+Riopradheep007@users.noreply.github.com>
2021-08-12 14:52:19 +0530

    Update remove_dublicates_element_array.py

--- Easy/remove_dublicates_element_array.py
+++ Easy/remove_dublicates_element_array.py
@@ -5,7 +5,7 @@
                                                         Remove Dublicates from the array 
                                                     #######################################
 
-                                                    """
+"""                                                    """
 
      Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
 
@@ -39,7 +39,8 @@ Output: 2, nums = [1,2,_]
 Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
 It does not matter what you leave beyond the returned k (hence they are underscores).
 
-
+"""
+  
 def removeDuplicates(nums):
 
     counter = 1
