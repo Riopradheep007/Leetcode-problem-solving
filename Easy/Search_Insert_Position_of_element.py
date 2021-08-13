36ca6da90e3083067063484ebe936c7d0988db39
Pradheep <46066018+Riopradheep007@users.noreply.github.com>
2021-08-13 19:12:28 +0530

    Update Search_Insert_Position_of_element.py

--- Easy/Search_Insert_Position_of_element.py
+++ Easy/Search_Insert_Position_of_element.py
@@ -1,4 +1,4 @@
-   
+  """ 
 	Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
 
 You must write an algorithm with O(log n) runtime complexity.
@@ -13,7 +13,7 @@ Example 2:
 
 Input: nums = [1,3,5,6], target = 2
 Output: 1
-
+"""
 
 
 def searchInsert(nums,target):
