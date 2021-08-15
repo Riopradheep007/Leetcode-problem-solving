7c5f8c5d2ac2486de6e46feb942a42ba60d862de
Pradheep <46066018+Riopradheep007@users.noreply.github.com>
2021-08-15 18:11:51 +0530

    Update merge_sorted_array.py

--- Easy/merge_sorted_array.py
+++ Easy/merge_sorted_array.py
@@ -1,4 +1,4 @@
-   
+ """
       You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
 
 Merge nums1 and nums2 into a single array sorted in non-decreasing order.
@@ -19,7 +19,7 @@ Input: nums1 = [1], m = 1, nums2 = [], n = 0
 Output: [1]
 Explanation: The arrays we are merging are [1] and [].
 The result of the merge is [1].
-
+"""
 
 def merge(nums1, m, nums2, n):
 
