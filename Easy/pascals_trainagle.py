fb390d19e76f8eb33870503a87dd48218537d8f1
Pradheep <46066018+Riopradheep007@users.noreply.github.com>
2021-08-16 19:26:41 +0530

    Update pascals_trainagle.py

--- Easy/pascals_trainagle.py
+++ Easy/pascals_trainagle.py
@@ -1,4 +1,4 @@
-
+"""
      Given an integer numRows, return the first numRows of Pascal's triangle.
 
 In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
@@ -14,6 +14,7 @@ Example 2:
 
 Input: numRows = 1
 Output: [[1]]
+"""
 
 def generate( numRows):
     result = []
