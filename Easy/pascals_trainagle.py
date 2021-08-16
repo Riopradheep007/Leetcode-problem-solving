b072cc298e8e064abcf57c4e5e4cd60ecb7f8c0d
Pradheep <46066018+Riopradheep007@users.noreply.github.com>
2021-08-16 19:29:29 +0530

    Update pascals_trainagle.py

--- Easy/pascals_trainagle.py
+++ Easy/pascals_trainagle.py
@@ -7,7 +7,11 @@
 In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
 
 
- 
+                  1
+                 1 1
+               1  2  1
+             1   3  3  1
+           1  4   6   4   1
 
 Example 1:
 
