edac9083a9a9503ef241077af7c606368e9cb916
Pradheep <46066018+Riopradheep007@users.noreply.github.com>
2021-08-17 20:21:35 +0530

    Update remove_Particular_Elements_In_Array.py

--- Easy/remove_Particular_Elements_In_Array.py
+++ Easy/remove_Particular_Elements_In_Array.py
@@ -1,4 +1,7 @@
-   """"
+ """   
+                                     ##################################################################
+                                              Remove particular element in a array
+                                     ##################################################################
    
     Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The relative order of the elements may be changed.
 
