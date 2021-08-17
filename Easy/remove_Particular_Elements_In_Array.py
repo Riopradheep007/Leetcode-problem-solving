f8a6bd7c0ac70f93972334eaad1d96a8c76728c0
Pradheep <46066018+Riopradheep007@users.noreply.github.com>
2021-08-17 20:20:15 +0530

    Update remove_Particular_Elements_In_Array.py

--- Easy/remove_Particular_Elements_In_Array.py
+++ Easy/remove_Particular_Elements_In_Array.py
@@ -1,4 +1,5 @@
-    
+   """"
+   
     Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The relative order of the elements may be changed.
 
 Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
@@ -34,7 +35,7 @@ Output: 2, nums = [2,2,_,_]
 Explanation: Your function should return k = 2, with the first two elements of nums being 2.
 It does not matter what you leave beyond the returned k (hence they are underscores).
 
-
+"""
 
 
 def removeElement(nums,val) :
