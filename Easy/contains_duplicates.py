b20d8987b1c751f13eb49e1b533c78e5f0172a68
Pradheep <46066018+Riopradheep007@users.noreply.github.com>
2021-08-23 21:20:25 +0530

    Update contains_duplicates.py

--- Easy/contains_duplicates.py
+++ Easy/contains_duplicates.py
@@ -1,5 +1,7 @@
 """
-
+                     ################################################################################3
+                                                 Contains duplicate
+                     ################################################################################3
     Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
  
