a5438a8301ce422191599f06e425f006a9be57ac
Pradheep <46066018+Riopradheep007@users.noreply.github.com>
2021-08-23 21:19:03 +0530

    Update contains_duplicates.py

--- Easy/contains_duplicates.py
+++ Easy/contains_duplicates.py
@@ -1,4 +1,4 @@
-
+"""
 
     Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
@@ -16,7 +16,7 @@ Example 3:
 
 Input: nums = [1,1,1,3,3,4,3,2,4,2]
 Output: true
-
+"""
 
 def containsDuplicate(nums):
     return len(nums)!=len(set(nums))
