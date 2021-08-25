0641452bd5c397d07380411b258ac2751f5b73a6
Pradheep <46066018+Riopradheep007@users.noreply.github.com>
2021-08-25 20:15:57 +0530

    Update max_consective_ones.py

--- Easy/max_consective_ones.py
+++ Easy/max_consective_ones.py
@@ -1,4 +1,5 @@
-Given a binary array nums, return the maximum number of consecutive 1's in the array.
+
+1.Given a binary array nums, return the maximum number of consecutive 1's in the array.
 
  
 
