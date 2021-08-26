217b0e4149cf56497fe2fe182ad883721585c90c
Pradheep <46066018+Riopradheep007@users.noreply.github.com>
2021-08-26 16:20:12 +0530

    Update teemo_attacking.py

--- Easy/teemo_attacking.py
+++ Easy/teemo_attacking.py
@@ -1,3 +1,5 @@
+ """
+ 
    Our hero Teemo is attacking an enemy Ashe with poison attacks! When Teemo attacks Ashe, Ashe gets poisoned for a exactly duration seconds. More formally, an attack at second t will mean Ashe is poisoned during the inclusive time interval [t, t + duration - 1]. If Teemo attacks again before the poison effect ends, the timer for it is reset, and the poison effect will end duration seconds after the new attack.
 
 You are given a non-decreasing integer array timeSeries, where timeSeries[i] denotes that Teemo attacks Ashe at second timeSeries[i], and an integer duration.
@@ -24,7 +26,7 @@ Explanation: Teemo's attacks on Ashe go as follows:
 Ashe is poisoned for seconds 1, 2, and 3, which is 3 seconds in total.
 
 
-
+"""
 
 class Solution:
     def findPoisonedDuration(self, timeSeries: List[int], duration: int) -> int:
