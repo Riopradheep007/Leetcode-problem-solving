ee73d4778f76557bdb974b1cd10d0468db67a53c
Pradheep <46066018+Riopradheep007@users.noreply.github.com>
2021-08-27 19:21:41 +0530

    Update assign_cookies.py

--- Easy/assign_cookies.py
+++ Easy/assign_cookies.py
@@ -1,4 +1,5 @@
-Assume you are an awesome parent and want to give your children some cookies. But, you should give each child at most one cookie.
+"""
+  Assume you are an awesome parent and want to give your children some cookies. But, you should give each child at most one cookie.
 
 Each child i has a greed factor g[i], which is the minimum size of a cookie that the child will be content with; and each cookie j has a size s[j]. If s[j] >= g[i], we can assign the cookie j to the child i, and the child i will be content. Your goal is to maximize the number of your content children and output the maximum number.
 
@@ -19,7 +20,7 @@ Explanation: You have 2 children and 3 cookies. The greed factors of 2 children 
 You have 3 cookies and their sizes are big enough to gratify all of the children, 
 You need to output 2.
 
-
+"""
 
 def findContentChildren(g, s):
     g,s = sorted(g),sorted(s)
