a3c88803c764f98c537acea7f43f38a1edd11e2d
Pradheep <46066018+Riopradheep007@users.noreply.github.com>
2021-08-28 11:41:24 +0530

    Update next_greater_element.py

--- Easy/next_greater_element.py
+++ Easy/next_greater_element.py
@@ -1,4 +1,5 @@
-  
+"""
+
    The next greater element of some element x in an array is the first greater element that is to the right of x in the same array.
 
 You are given two distinct 0-indexed integer arrays nums1 and nums2, where nums1 is a subset of nums2.
@@ -24,7 +25,7 @@ Output: [3,-1]
 Explanation: The next greater element for each value of nums1 is as follows:
 - 2 is underlined in nums2 = [1,2,3,4]. The next greater element is 3.
 - 4 is underlined in nums2 = [1,2,3,4]. There is no next greater element, so the answer is -1.
-
+"""
 
 
 
