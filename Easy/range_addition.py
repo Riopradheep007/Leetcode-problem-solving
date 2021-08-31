6a0e22242e2bbde3762aee0a40c9b6bc01de7c2e
Pradheep <46066018+Riopradheep007@users.noreply.github.com>
2021-08-31 14:51:36 +0530

    Update range_addition.py

--- Easy/range_addition.py
+++ Easy/range_addition.py
@@ -1,4 +1,4 @@
-
+"""
    You are given an m x n matrix M initialized with all 0's and an array of operations ops, where ops[i] = [ai, bi] means M[x][y] should be incremented by one for all 0 <= x < ai and 0 <= y < bi.
 
 Count and return the number of maximum integers in the matrix after performing all the operations.
@@ -20,6 +20,7 @@ Example 3:
 Input: m = 3, n = 3, ops = []
 Output: 9
 
+"""
 
 class Solution:
     def maxCount(self, m: int, n: int, ops: List[List[int]]) -> int:
