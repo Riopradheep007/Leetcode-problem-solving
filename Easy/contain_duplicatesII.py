e2db586196c4e24afe16f9557a0f0257333c9bee
Pradheep <46066018+Riopradheep007@users.noreply.github.com>
2021-09-01 20:31:03 +0530

    Update contain_duplicatesII.py

--- Easy/contain_duplicatesII.py
+++ Easy/contain_duplicatesII.py
@@ -1,4 +1,5 @@
-  Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+ """ 
+	Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
 
  
 
@@ -14,7 +15,7 @@ Example 3:
 
 Input: nums = [1,2,3,1,2,3], k = 2
 Output: false
-
+"""
 
 
 
