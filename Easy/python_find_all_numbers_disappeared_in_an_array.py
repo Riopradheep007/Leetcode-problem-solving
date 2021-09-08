9068f6d7e302356d095e380ad7d8230af0897cc2
Pradheep <46066018+Riopradheep007@users.noreply.github.com>
2021-09-08 19:31:16 +0530

    Add files via upload

---
+++ Easy/python_find_all_numbers_disappeared_in_an_array.py
@@ -0,0 +1,21 @@
+Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+
+ 
+
+Example 1:
+
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [5,6]
+Example 2:
+
+Input: nums = [1,1]
+Output: [2]
+
+
+def findDisappearedNumbers( nums):
+    result = [* range(0, len(nums)+1)] 
+    for i in nums: 
+        result[i] = 0
+    return [i for i in result if i != 0]
+    
+print(findDisappearedNumbers([4,3,2,7,8,2,3,1]))
