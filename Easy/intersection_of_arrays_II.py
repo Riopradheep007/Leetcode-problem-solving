d3cec9537d9b2176ca9a3f94e4d40e22c3598a2c
Pradheep <46066018+Riopradheep007@users.noreply.github.com>
2021-09-10 20:17:14 +0530

    Update intersection_of_arrays_II.py

--- Easy/intersection_of_arrays_II.py
+++ Easy/intersection_of_arrays_II.py
@@ -1,4 +1,4 @@
-    
+"""    
     Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
 
  
@@ -13,7 +13,7 @@ Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
 Output: [4,9]
 Explanation: [9,4] is also accepted.
 
-
+"""
 
 from collections import Counter
 def intersect(nums1,nums2):
