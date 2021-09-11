f61926fde7cac925a74b08df6e084b3794b85b4a
Pradheep <46066018+Riopradheep007@users.noreply.github.com>
2021-09-11 15:42:51 +0530

    Update sort_array_by_parityII.py

--- Easy/sort_array_by_parityII.py
+++ Easy/sort_array_by_parityII.py
@@ -1,6 +1,8 @@
 
 """
-
+                      ###########################################################################################3
+                                                        Sort Array By ParityII
+                      ############################################################################################
     Given an array of integers nums, half of the integers in nums are odd, and the other half are even.
 
 Sort the array so that whenever nums[i] is odd, i is odd, and whenever nums[i] is even, i is even.
