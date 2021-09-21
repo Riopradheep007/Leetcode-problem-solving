195f4801489102e6ab74bc69be6963d02df56696
Pradheep <46066018+Riopradheep007@users.noreply.github.com>
2021-09-21 19:37:01 +0530

    Update 1002_find_common_characters.py

--- Easy/1002_find_common_characters.py
+++ Easy/1002_find_common_characters.py
@@ -1,4 +1,4 @@
-
+"""
 
     Given a string array words, return an array of all characters that show up in all strings within the words (including duplicates). You may return the answer in any order.
 
@@ -12,7 +12,7 @@ Example 2:
 
 Input: words = ["cool","lock","cook"]
 Output: ["c","o"]
-
+"""
 
 class Solution:
     def commonChars(self, words: List[str]) -> List[str]:
