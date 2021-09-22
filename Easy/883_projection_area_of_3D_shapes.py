14731b011ae5f7807254d08fc151596ec183dc86
Pradheep <46066018+Riopradheep007@users.noreply.github.com>
2021-09-22 20:08:55 +0530

    Update 883_projection_area_of_3D_shapes.py

--- Easy/883_projection_area_of_3D_shapes.py
+++ Easy/883_projection_area_of_3D_shapes.py
@@ -1,5 +1,6 @@
 """                           
                   ####################################################################################################
+                                                       Projection  Area of 3D shape
                   ####################################################################################################
      You are given an n x n grid where we place some 1 x 1 x 1 cubes that are axis-aligned with the x, y, and z axes.
 
