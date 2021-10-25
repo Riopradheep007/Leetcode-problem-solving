fa98664c12cb65faa54894ce76b344044452cc35
Pradheep <46066018+Riopradheep007@users.noreply.github.com>
2021-10-25 20:38:06 +0530

    Update 2032_two_out_of_three.py

--- Easy/2032_two_out_of_three.py
+++ Easy/2032_two_out_of_three.py
@@ -1,4 +1,4 @@
-  
+"""  
   Given three integer arrays nums1, nums2, and nums3, return a distinct array containing all the values that are present in at least two out of the three arrays. You may return the values in any order.
  
 
@@ -28,6 +28,7 @@ Constraints:
 
 1 <= nums1.length, nums2.length, nums3.length <= 100
 1 <= nums1[i], nums2[j], nums3[k] <= 100
+"""
 
 from collections import Counter
 class Solution:
